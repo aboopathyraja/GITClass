15f99c7f7a562bf01814dc95632c245cbb4707a1
aboopathyraja <aboopathyraja@gmail.com>
2022-04-03 10:47:17 +0530

    method added

--- src/test/java/com/adactin/Git.java
+++ src/test/java/com/adactin/Git.java
@@ -1,5 +1,25 @@
 package com.adactin;
 
 public class Git {
+	
+	public void Test1() {
+		System.out.println("chennai");
+	}
+	
+	public void Test2() {
+		System.out.println("coimbatore");
+	}
+	
+	public void Test3() {
+		System.out.println("Namakkal");
+	}
+	
+	public void Test4() {
+		System.out.println("Salem");
+	}
+	
+	
+	
+	
 
 }
